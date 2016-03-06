1ab7b159bf77b6c89cf1bb04ba5bd49969805268
Kalle Lyttinen <kalle.lyttinen@mail.student.oulu.fi>
2016-03-06 19:03:10 +0200

    finished users table unit tests

--- test/database_api_tests_users.py
+++ test/database_api_tests_users.py
@@ -15,14 +15,13 @@ USER1_PRIVILEGES = 1
 INSERT INTO "users" VALUES(1,'Axel f.','Axel Foley','a.foley@lapd.com',1);
 '''
 
-
 NEW_USER = {'nicname': 'Commando',
             'realname': 'John Matrix',
             'email': 'secretmail@logcabin.com',
             'privilege_level' : 1
             }
 USER_WRONG_NICKNAME = 'Batty'
-INITIAL_SIZE = 5
+INITIAL_SIZE = 4
 
 
 class UserDBAPITestCase(unittest.TestCase):
@@ -70,7 +69,7 @@ class UserDBAPITestCase(unittest.TestCase):
 
 
         print '('+self.test_get_user.__name__+')', \
-              self.test_get_user.__doc__
+            self.test_get_user.__doc__
 
         #Test with an existing user
         user = self.connection.get_user(USER1_ID)
@@ -86,15 +85,77 @@ class UserDBAPITestCase(unittest.TestCase):
         self.assertEqual(email, USER1_EMAIL)
         self.assertEqual(privileges, USER1_PRIVILEGES)
 
+    def test_add_user(self):
+        '''
+        tests add_user(self, nickname, real_name, email, privilege_level function
+        '''
 
-    '''
-        Test these functions
-        def add_user(self, nickname, real_name, email, privilege_level):
-        def modify_user(self, user_ID, username=None, realname=None, email=None, privilege_level=None):
-        def get_user(self, ID):
-        def delete_user(self, ID):
-        def get_users(self, upperlimit=None, offset=0):
-    '''
+        result = self.connection.add_user(NEW_USER['nicname'], NEW_USER['realname'], NEW_USER['email'], NEW_USER['privilege_level']);
+        self.assertNotEqual(result, False)
+
+        #Test with an new user
+        user = self.connection.get_user(result)
+        realname = user.get('realname')
+        nickname = user.get('username')
+        email = user.get('email')
+        privileges = user.get('privilege_level')
+
+        self.assertEqual(realname, NEW_USER['realname'])
+        self.assertEqual(nickname, NEW_USER['nicname'])
+        self.assertEqual(email, NEW_USER['email'])
+        self.assertEqual(privileges, NEW_USER['privilege_level'])
+
+
+    def test_modify_user(self):
+        '''
+        test  modify_user(self, user_ID, username=None, realname=None, email=None, privilege_level=None)
+        '''
+
+        # get info from existing user
+        user = self.connection.get_user(USER1_ID)
+        gotid = user.get('user_id')
+        realname = user.get('realname')
+        nickname = user.get('username')
+        email = user.get('email')
+        privileges = user.get('privilege_level')
+
+        # modify that info
+        self.connection.modify_user(USER1_ID,USER1_NICKNAME+"_MOD",USER1_REAL_NAME+"_MOD",USER1_EMAIL+"_MOD")
+
+        user2 = self.connection.get_user(USER1_ID)
+
+        # get info again to see it actually changed
+        self.assertEqual(user2['user_id'],USER1_ID)
+        self.assertEqual(user2['realname'],USER1_REAL_NAME+"_MOD")
+        self.assertEqual(user2['username'],USER1_NICKNAME+"_MOD")
+        self.assertEqual(user2['email'],USER1_EMAIL+"_MOD")
+
+        # revert changes
+        self.connection.modify_user(USER1_ID,USER1_NICKNAME,USER1_REAL_NAME,USER1_EMAIL)
+
+    def test_delete_user(self):
+        '''
+        tests         def delete_user(self, ID)
+        '''
+        user = self.connection.get_user(USER1_ID)
+        self.assertNotEqual(user, None)
+        self.connection.delete_user(USER1_ID)
+        user = self.connection.get_user(USER1_ID)
+        self.assertEqual(user,None)
+
+    def test_wrong_ID(self):
+        user = self.connection.get_user(8321439824)
+        self.assertEqual(user,None)
+
+'''
+    Discussion needed about how get_users() should actually behave
+    def test_get_users(self):
+
+        users = self.connection.get_users()
+
+        self.assertEqual(users.)
+'''
+
 
 if __name__ == '__main__':
     print 'Start running user tests'

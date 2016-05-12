0d9e675f31f8d660f8f67f277d26e7715353e442
Kalle Lyttinen <kalle.lyttinen@mail.student.oulu.fi>
2016-05-12 11:58:39 +0300

    Revert "moved single user resource into not implemented"
    This reverts commit 1df7ca0f231fc2e420052133dfa0d03352b60a8a.

--- jukebox/resources.py
+++ jukebox/resources.py
@@ -199,6 +199,14 @@ class Users(Resource):
         g.con.add_user(username, real_name, email, 0)
         return Response(string_data, 201, mimetype="text/html")
 
+class User(Resource):
+    def get(self):
+        abort(404)
+
+    def post(self):
+        abort(404)
+
+
 class Songs(Resource):
     '''
     Resource Songs implementation
@@ -739,6 +747,8 @@ class Chat(Resource):
 #Define the routes
 api.add_resource(Users, '/sangz/api/users/',
                  endpoint='users')
+api.add_resource(User, '/sangz/api/users/<userid>',
+                 endpoint = 'user')
 api.add_resource(Frontpage, '/sangz/api/',
                  endpoint='')
 api.add_resource(Playlist, '/sangz/api/playlist/',
@@ -756,8 +766,6 @@ api.add_resource(Votes, '/sangz/api/votes/',
 # won't be implemented
 # api.add_resource(Artists, '/sangz/api/artists/',
 #                  endpoint='artists')
-#api.add_resource(User, '/sangz/api/users/<userid>',
-#                 endpoint = 'user')
 # api.add_resource(Artist, '/sangz/api/artists/<artistid>/',
 #                  endpoint='artist')
 # api.add_resource(Albums, '/sangz/api/albums/',
